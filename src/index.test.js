f8ea6442e3eff6e61236aefe5ec0cd53960e7455
Chris <mdhornet90@gmail.com>
2019-03-04 23:43:42 -0800

    Finishing unit tests

--- src/index.test.js
+++ src/index.test.js
@@ -75,17 +75,109 @@ describe('Route Builder Tests', () => {
       test(`The builder will generate route information for any files whose name matches "${method}"`, async () => {
         await fsify(
           translateDirectoryStructure({
-            [`someFolder/api/endpoints/foo/bar/${method}.js`]: 'module.exports = { default: () => {} }',
+            'someFolder/api/endpoints': {
+              'users.js': 'module.exports = { get: () => {} }',
+              'foo/bar': {
+                [`${method}.js`]: 'module.exports = { default: () => {} }',
+                'thiswontmatch.js': 'module.exports = { default: () => {} }',
+              },
+            },
           }),
         );
 
-        expect(routeBuilder(TESTING_DIRECTORY)).toMatchObject([
-          {
-            method,
-            route: '/foo/bar',
-            handlingFunction: expect.any(Function),
+        const routes = routeBuilder(TESTING_DIRECTORY);
+        expect(routes).toContainEqual({
+          method,
+          route: '/foo/bar',
+          handlingFunction: expect.any(Function),
+        });
+        expect(routes).toContainEqual({
+          method: 'get',
+          route: '/users',
+          handlingFunction: expect.any(Function),
+        });
+      });
+    });
+
+    test('The builder by default will not generate routes for any files that contain exports other than http methods', async () => {
+      await fsify(
+        translateDirectoryStructure({
+          'someFolder/api/endpoints/foo/bar': {
+            [`something.js`]: 'module.exports = { get: () => {}, foo: () => {} }',
+            'thiswontmatch.js': 'module.exports = { default: () => {} }',
           },
-        ]);
+        }),
+      );
+
+      expect(routeBuilder(TESTING_DIRECTORY)).toEqual([]);
+    });
+
+    test('The builder will by default generate routes for any files that contain only http methods as exports', async () => {
+      await fsify(
+        translateDirectoryStructure({
+          'someFolder/api/endpoints/foo/bar': {
+            [`baz.js`]: 'module.exports = { get: () => {}, post: () => {} }',
+            'thiswontmatch.js': 'module.exports = { default: () => {} }',
+          },
+        }),
+      );
+
+      const routes = routeBuilder(TESTING_DIRECTORY);
+      expect(routes).toContainEqual({
+        method: 'get',
+        route: '/foo/bar/baz',
+        handlingFunction: expect.any(Function),
+      });
+      expect(routes).toContainEqual({
+        method: 'post',
+        route: '/foo/bar/baz',
+        handlingFunction: expect.any(Function),
+      });
+    });
+
+    test('The builder can generate routes from index.js using the folder name as the path', async () => {
+      await fsify(
+        translateDirectoryStructure({
+          'someFolder/api/endpoints/foo/bar/baz': {
+            [`index.js`]: 'module.exports = { get: () => {}, post: () => {} }',
+            'thiswontmatch.js': 'module.exports = { default: () => {} }',
+          },
+        }),
+      );
+
+      const routes = routeBuilder(TESTING_DIRECTORY);
+      expect(routes).toContainEqual({
+        method: 'get',
+        route: '/foo/bar/baz',
+        handlingFunction: expect.any(Function),
+      });
+      expect(routes).toContainEqual({
+        method: 'post',
+        route: '/foo/bar/baz',
+        handlingFunction: expect.any(Function),
+      });
+    });
+
+    test('The builder will detect underscore-prefixed folders as route parameters', async () => {
+      await fsify(
+        translateDirectoryStructure({
+          'someFolder/api/endpoints/users/_id/stuff': {
+            [`index.js`]: 'module.exports = { get: () => {}, post: () => {} }',
+            'thiswontmatch.js': 'module.exports = { default: () => {} }',
+          },
+        }),
+      );
+
+      const routes = routeBuilder(TESTING_DIRECTORY);
+      expect(routes).toContainEqual({
+        method: 'get',
+        route: '/users/:id/stuff',
+        handlingFunction: expect.any(Function),
+      });
+      expect(routes).toContainEqual({
+        method: 'post',
+        route: '/users/:id/stuff',
+        handlingFunction: expect.any(Function),
       });
     });
   });
