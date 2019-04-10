4897c2401ae916577ec8298fe7421a17af7a6776
Chris <mdhornet90@gmail.com>
2019-04-09 22:56:24 -0700

    Enabling dashboard reporter

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,7 +1,7 @@
 module.exports = config => {
   config.set({
     packageManager: 'yarn',
-    reporters: ['progress', 'html', 'clear-text'],
+    reporters: ['progress', 'html', 'clear-text', 'dashboard'],
     testRunner: 'jest',
     coverageAnalysis: 'off',
     thresholds: { break: 75 },
